cfe15cc9ea3aa6e115f998be4ae1d30ee1ef2689
fernandobl-046 <fernandoluis.lucas@gmail.com>
2020-12-25 16:42:23 -0300

    adicionado página de contato e configurado banco de dados

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    data_modificado = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract=True
+
+class Contato(BaseModel):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField()
+    conteudo = models.TextField()
+
+    def __str__(self):
+        return self.nome
+    
--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.urls import path
+from django.http import JsonResponse
+from core.models import Contato
 
 
 def home(request):
@@ -27,11 +29,21 @@ def empresa(request):
 
 
 def contato(request):
-    latest_question_list = "Django"
-    context = {
-        'teste': latest_question_list,
-    }
-    return render(request, 'contato.html', context)
+    if request.method == 'GET':
+        contatos = Contato.objects.all()        
+        context = {
+            'contatos': contatos,
+        }
+        return render(request, 'contato.html', context)
+    if request.method == 'POST':
+        try:
+            nome = request.POST.get('nome')
+            email = request.POST.get('email')
+            comment =  request.POST.get('comment')
+            Contato.objects.create(nome=nome, email=email, conteudo=comment)
+            return JsonResponse("Gravou", safe=False)
+        except Exception as e:
+            return JsonResponse("Erro" + e, safe=False)
 
     
 
