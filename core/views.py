0aa67600a2ae592bc8d0e36da57c2e009dc8be46
fernandobl-046 <fernandoluis.lucas@gmail.com>
2020-12-25 15:19:21 -0300

    criado pagina em bootstrap

--- core/views.py
+++ core/views.py
@@ -10,4 +10,29 @@ def home(request):
     return render(request, 'index.html', context)
 
 
+def produtos(request):
+    latest_question_list = "Django"
+    context = {
+        'teste': latest_question_list,
+    }
+    return render(request, 'produtos.html', context)
+
+
+def empresa(request):
+    latest_question_list = "Django"
+    context = {
+        'teste': latest_question_list,
+    }
+    return render(request, 'empresa.html', context)
+
+
+def contato(request):
+    latest_question_list = "Django"
+    context = {
+        'teste': latest_question_list,
+    }
+    return render(request, 'contato.html', context)
+
+    
+
     
--- estudos_django/settings.py
+++ estudos_django/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fontawesome-free',
 ]
 
 MIDDLEWARE = [
@@ -106,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -121,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
