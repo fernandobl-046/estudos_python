e4414d577896efdb9a17823b7f992203b90df7df
fernandobl-046 <fernandoluis.lucas@gmail.com>
2021-01-16 19:25:06 -0300

    aprendendo a usar classmethod python

--- core/views.py
+++ core/views.py
@@ -46,38 +46,51 @@ def logar(request):
         else:
             return render(request, 'logar.html', {'erro': 'Não foi possível logar'})
 
-def calcular(request):
-    if request.method == 'GET':
-        total_desconto = 11.35
-        total_frete = 21.35
-        total_carrinho = 0.00
-        total_value = 0.00
 
-        list_items = [{
-                'product_name': 'Frete',
-                'quantity': 1,
-                'value_item': round(total_frete, 2)
-            }]
+class PagadorShipay:
 
-        for item in range(2):
-            list_items.append({
-                'product_name': 'Itens',
-                'quantity': 5,
-                'value_item': round(189.99, 2)
-            })
-            total_carrinho += round(189.99 * 5, 2)
-        desconto_percent = 1 - total_desconto / 100
-        if total_desconto > 0:
-            calcular_desconto = total_carrinho + total_frete - (total_carrinho + total_frete * desconto_percent)
-            calcular_desconto = round(calcular_desconto / len(list_items), 2)
+    def __init__(self, url_api, total_frete=0):
+       self.url_api = url_api
+       self.total_frete = total_frete
+
+    @staticmethod
+    def convert_to_float(number):
+            return float(number)
+
+    @classmethod
+    def calcular_carrinho(cls, request):
+        if request.method == 'GET':
+            total_desconto = 11.35
+            total_frete = cls.convert_to_float(request.GET.get('frete'))
+            total_carrinho = 0.00
+            total_value = 0.00
+
+            list_items = [{
+                    'product_name': 'Frete',
+                    'quantity': 1,
+                    'value_item': round(total_frete, 2)
+                }]
+
+            for item in range(2):
+                list_items.append({
+                    'product_name': 'Itens',
+                    'quantity': 5,
+                    'value_item': round(189.99, 2)
+                })
+                total_carrinho += round(189.99 * 5, 2)
+            desconto_percent = 1 - total_desconto / 100
+            if total_desconto > 0:
+                calcular_desconto = total_carrinho + total_frete - (total_carrinho + total_frete * desconto_percent)
+                calcular_desconto = round(calcular_desconto / len(list_items), 2)
+                for item in list_items:
+                    desconto_item = round(item['value_item'] - calcular_desconto, 2)
+                    item['value_item'] = desconto_item
+            
             for item in list_items:
-                desconto_item = round(item['value_item'] - calcular_desconto, 2)
-                item['value_item'] = desconto_item
-        
-        for item in list_items:
-            calcular_total = round(item['value_item'] * item['quantity'], 2)
-            total_value += calcular_total
-        print(list_items)
-        print("{:.2f}".format(total_value))
+                calcular_total = round(item['value_item'] * item['quantity'], 2)
+                total_value += calcular_total
+            print(list_items)
+            print("{:.2f}".format(total_value))
 
-        return JsonResponse('teste', status=200, safe=False)
+            return JsonResponse('teste', status=200, safe=False)
+        
--- estudos_django/urls.py
+++ estudos_django/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from django.http import request
 from django.urls import path
 from core import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('index/calcular', views.calcular, name='calcular'),
+    path('index/calcular', views.PagadorShipay.calcular_carrinho, name='calcular'),
     path('accounts/login/', views.logar, name='logar'),
     path('produtos/', views.produtos, name='produtos'),
     path('empresa/', views.empresa, name='empresa'),
