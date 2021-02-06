31506ce75a780d2072fd4944b5c892a6ebaeef97
fernandobl-046 <fernandoluis.lucas@gmail.com>
2021-02-06 08:53:36 -0300

    Teste branch

--- core/views.py
+++ core/views.py
@@ -8,14 +8,14 @@ from core.models import Contato
 from django.views import View
 
 
-def home(request):
-    return render(request, 'index.html')
+# def home(request):
+#     return render(request, 'index.html')
 
-def produtos(request):
-    return render(request, 'produtos.html')
+# def produtos(request):
+#     return render(request, 'produtos.html')
 
-def empresa(request):
-    return render(request, 'empresa.html')
+# def empresa(request):
+#     return render(request, 'empresa.html')
 
 @login_required
 def contato(request):
